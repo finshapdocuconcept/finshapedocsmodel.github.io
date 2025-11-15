585e352382f6f03e9c39cac797818376bad20ac3
yevab <yevab@Yevik>
2025-11-15 17:07:42 +0100

    web rename

--- oxygen-webhelp/app/options/properties.js
+++ oxygen-webhelp/app/options/properties.js
@@ -19,7 +19,7 @@ var properties =
   "webhelp.show.expand.collapse.sections" : "yes",
   "webhelp.show.full.size.image" : "true",
   "webhelp.show.indexterms.link" : "yes",
-  "webhelp.show.main.page.tiles" : "yes",
-  "webhelp.show.main.page.toc" : "no",
+  "webhelp.show.main.page.tiles" : "no",
+  "webhelp.show.main.page.toc" : "yes",
   "webhelp.top.menu.depth" : "3",
   "webhelp.topic.collapsible.elements.initial.state" : "expanded" };
